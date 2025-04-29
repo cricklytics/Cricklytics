cf8fb7d41f4ca0e09a25808b2883b97ca4d3bf3c
Cricklytics <rajesh.d@creativityventures.co.in>
2025-04-29 13:05:30 +0530

    routed properluy from 2025 page to kudos

--- src/components/pawan/Frame.jsx
+++ src/components/pawan/Frame.jsx
@@ -43,7 +43,7 @@ const Frame1321317519 = () => {
         src={FrameImage}
         alt="Frame 1321317522"
         loading="lazy"
-        className="absolute left-[1px] top-[30px] w-[40px] h-[53px] cursor-pointer  w-10 h-10 -scale-x-100 "onClick={() => navigate("/landingpage")}
+        className="absolute left-[1px] top-[30px] w-[40px] h-[53px] cursor-pointer  w-10 h-10 -scale-x-100 "onClick={() => navigate("/awards")}
       />
     </div>
   );
